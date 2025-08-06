2b54c0ebf9469bf4ceccc8d4d01713405318d5cf
sujal10022005 <144321575+sujal10022005@users.noreply.github.com>
2025-08-06 22:16:53 +0530

    Minor Changes in footer

--- app/layout.js
+++ app/layout.js
@@ -39,7 +39,7 @@ export default function RootLayout({ children }) {
          {/* footer */}
          <footer className="bg-muted/50 py-12">
           <div className="container mx-auto px-4 text-center text-gray-200">
-            <p>errfiebfb</p>
+            <p>Made By Sujal Nage</p>
           </div>
          </footer>
         </ThemeProvider>
