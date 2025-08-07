4ac6c735ca1f7d78c8c57159c4c64a40b003ef50
sujal10022005 <144321575+sujal10022005@users.noreply.github.com>
2025-08-07 11:02:49 +0530

    Minor Changes in Sign-up

--- app/(auth)/sign-up/[[...sign-up]]/page.jsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.jsx
@@ -2,7 +2,11 @@ import { SignUp } from '@clerk/nextjs'
 import React from 'react'
 
 const SignUpPage = () => {
-  return <SignUp />
+  return (
+    <div className="flex items-center justify-center min-h-screen  pt-20"><SignUp />
+    </div>
+    )
+
 };
 
 export default SignUpPage
--- app/(auth)/sign-up/layout.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-const AuthLayout = ({children}) => {
-  return <div className='flex justify-centre pt-40'>{children}</div>
-};
-
-export default AuthLayout
